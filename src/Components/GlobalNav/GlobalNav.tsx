9f85c89f8986f26ec1c836db9fab78fd4f0ba845
Devin <fitzsimonsdevin@gmail.com>
2022-07-04 15:49:42 -0600

    test key

--- src/Components/GlobalNav/GlobalNav.tsx
+++ src/Components/GlobalNav/GlobalNav.tsx
@@ -1,7 +1,7 @@
 export const GlobalNav = () => {
   return (
     <header className="bg-white  m-auto max-w-6xl">
-      <p>Hello Vite + React! {import.meta.env.VITE_TEST_KEY}</p>
+      <p>Hello Vite + React! {import.meta.env.VITE_GITHUB_ACCESS_KEY}</p>
     </header>
   );
 };
